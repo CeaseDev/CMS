37075eba4ede3f610dc8ecd133755777c456c3f6
AkBeast007 <aryankashyap0772@gmail.com>
2023-10-20 15:14:17 +0530

    Document Upload and Access

--- SIH/client/Case-Management-system-Frontend/src/App.jsx
+++ SIH/client/Case-Management-system-Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header'
-import{BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
+import{BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from './components/Home'
 import E_filling from './components/E_filling'
 import FooterBottom from './components/FooterBottom'
 import Login_Register from './components/Login_Register'
 import Document from './components/Document'
+import Access from './components/Access'
+import Upload from './components/Upload'
 
 function App() {
  
@@ -18,6 +20,8 @@ function App() {
           <Route path='/Home' element={<Home/>}/>
           <Route path='/E_Filling' element={<E_filling/>}/>
           <Route path='/Document' element={<Document/>}/>
+          <Route path='/Upload' element={<Upload/>}/>
+          <Route path='/Access' element={<Access/>}/>
           <Route path='/Login_register' element={<Login_Register/>}/>
       </Routes>
 
