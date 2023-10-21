b74bad6a7885c99c2186d9ad05a0d658df440b65
Akshat-07k <akshat070704@gmail.com>
2023-10-21 17:25:42 +0530

    Register integated

--- SIH/client/Case-Management-system-Frontend/src/components/Register.jsx
+++ SIH/client/Case-Management-system-Frontend/src/components/Register.jsx
@@ -1,59 +1,103 @@
-import React, {useState } from 'react' ; 
-import {EyeInvisibleOutlined, EyeOutlined} from "@ant-design/icons";
-
+import React, { useState } from 'react';
+import { EyeInvisibleOutlined, EyeOutlined } from "@ant-design/icons";
+import axios from "axios";
 function Register() {
-  const [user , setUser] = useState({
-    UID : '',  
-    fname : '' , 
-    lname : '' , 
-    email : '',
-    mobile : '', 
-    password : '',
-    retype : '', 
-    isMatched : true 
+  const [user, setUser] = useState({
+    UID: '',
+    fname: '',
+    lname: '',
+    email: '',
+    phone: '',
+    password: '',
+    retype: '',
+    isMatched: true
   })
 
 
   const handleSubmit = (e) => {
-    e.preventDefault() ; 
+    e.preventDefault();
+
+    if (user.password !== user.retype) {
+      setUser((prevUser) => ({
+        ...prevUser,
+        isMatched: false,
+      }));
+    }
+
+    else {
+      setUser((prevUser) => ({
+        ...prevUser,
+        isMatched: true,
+      }));
+      console.log(user, 1);
+
+      const body = {
+        uid: user.UID,
+        fname: user.fname,
+        lname: user.lname,
+        email: user.email,
+        mobile: user.phone,
+        password: user.password
+      }
+      console.log(body, 2);
+
+      axios.post('http://localhost:4000/api/v1/user/signup', body, { headers: { 'Content-Type': 'application/json' } })
+        .then(response => {
+
+          const token = response.data.token;
+
+          // Save the token in local storage
+          localStorage.setItem('token', token);
+        })
+        .catch(error => {
+          // Handle any errors here
+          console.error('Error:', error);
+        });
+    }
+
+
   }
 
-  const changeConfirmPasswordHandler = (e) =>{
-    setUser( {retype : e.target.value}) ; 
+  const changeConfirmPasswordHandler = (e) => {
+    const newConfirmPassword = e.target.value;
+    setUser((prevUser) => ({
+      ...prevUser,
+      retype: newConfirmPassword,
+    }));
   }
 
-  const changePasswordHandler = (e) =>{
-    setUser( {password : e.target.value}) ; 
-    const {password , retype } = user ; 
-    console.log(password) ; 
-    console.log(retype ) ; 
-    const match = password === retype ; 
-    setUser( {isMatched : match }) ; 
+  const changePasswordHandler = (e) => {
+    const newPassword = e.target.value;
+    setUser((prevUser) => ({
+      ...prevUser,
+      password: newPassword,
+    }));
   }
 
 
   const handleInput = (e) => {
     const { name, value } = e.target;
-    setUser( (prevData) =>{
-      return {  
+    console.log(name, value);
+    setUser((prevData) => {
+      return {
         ...prevData,
         [name]: value
-        }
-      }  ) ; 
-    }
+      }
+    });
+  }
 
-     const [visible,setVisible ] = useState(false);
-     const [visible1,setVisible1 ] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const [visible1, setVisible1] = useState(false);
 
 
   return (
     <>
-        <form className='m-3' onSubmit={handleSubmit}>
+      <form className='m-3' onSubmit={handleSubmit}>
         <div className="space-y-12">
           <div className="border-b border-gray-900/10 pb-2">
 
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <h1 className='text-3xl text-left'>Personal Information</h1>
+              <h1 className='text-3xl text-left'>Personal Information</h1>
             </div>
           </div>
           <div className="border-b border-gray-900/10 pb-6">
@@ -96,53 +140,53 @@ function Register() {
 
           </div>
           <div className="border-b border-gray-900/10 pb-2">
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-3">
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
                 <div className="flex mt-2">
-                
+
                   <input name="password" placeholder="Password" value={user.password}
-                   onChange={changePasswordHandler}  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
-                   title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
-                   id="password" className="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
-                   type = {visible ? "text": "password"}/>
+                    onChange={changePasswordHandler}
+                    title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
+                    id="password" className="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    type={visible ? "text" : "password"} />
 
-                   <div className = 'p-2' onClick = {() => setVisible(!visible)}>
+                  <div className='p-2' onClick={() => setVisible(!visible)}>
                     {visible ? <EyeOutlined /> : <EyeInvisibleOutlined />}
-                   </div>
-                   
-                </div>
-                
-            </div>
+                  </div>
 
-            <div className="grid-column-start: 1 sm:col-span-3">
+                </div>
+
+              </div>
+
+              <div className="grid-column-start: 1 sm:col-span-3">
                 <label htmlFor="retype" className="block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
                 <div className="flex mt-2">
-                
-                  <input name="retype" placeholder="Confirm Password" value={user.retype}
-                   onChange={changeConfirmPasswordHandler}  
-                   id="pass" className="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" 
-                   type = {visible1 ? "text": "password"}/>
 
-                   <div className = 'p-2' onClick = {() => setVisible1(!visible1)}>
-                   {!user.isMatched && (
-                        <p style={{ color: 'red' }}>Passwords do not match.</p>
+                  <input name="retype" placeholder="Confirm Password" value={user.retype}
+                    onChange={changeConfirmPasswordHandler}
+                    id="pass" className="block pl-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    type={visible1 ? "text" : "password"} />
+
+                  <div className='p-2' onClick={() => setVisible1(!visible1)}>
+                    {!user.isMatched && (
+                      <p style={{ color: 'red' }}>Passwords do not match.</p>
                     )}
                     {visible1 ? <EyeOutlined /> : <EyeInvisibleOutlined />}
-                   </div>
-                   
-                </div>
-                
-            </div>
+                  </div>
 
-          </div>
+                </div>
+
+              </div>
+
+            </div>
           </div>
           <div className="mt-6 flex items-center justify-center gap-x-6">
-                <button type="button" className="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-                <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign Up</button>
+            <button type="button" className="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+            <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign Up</button>
           </div>
         </div>
-      </form> 
+      </form>
     </>
   )
 }
